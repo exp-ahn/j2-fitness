080c30e3d010eae69226738c8b50a3d8d79275ac
exp-ahn <wltn24996@gmail.com>
2024-10-12 09:53:42 +0900

    migration to vue2

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
+    <h1>testtesttest</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br>
       check out the
@@ -33,15 +33,11 @@
 </template>
 
 <script lang="ts">
-import { Options, Vue } from 'vue-class-component';
+import { Component, Prop, Vue } from 'vue-property-decorator';
 
-@Options({
-  props: {
-    msg: String
-  }
-})
+@Component
 export default class HelloWorld extends Vue {
-  msg!: string
+  @Prop() private msg!: string;
 }
 </script>
 
