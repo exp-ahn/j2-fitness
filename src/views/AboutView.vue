ad9e04447093a21750c61c377da303ef3c79cbeb
exp-ahn <wltn24996@gmail.com>
2024-10-13 17:51:12 +0900

    "fixed vue compile options, add public imgs, fixed dependences"

--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,16 +1,25 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <button type="button" class="btn btn-primary">Primary</button>
+    <button type="button" class="btn btn-secondary">Secondary</button>
+    <button type="button" class="btn btn-success">Success</button>
+    <button type="button" class="btn btn-danger">Danger</button>
+    <button type="button" class="btn btn-warning">Warning</button>
+    <button type="button" class="btn btn-info">Info</button>
+    <button type="button" class="btn btn-light">Light</button>
+    <button type="button" class="btn btn-dark">Dark</button>
+    <button type="button" class="btn btn-link">Link</button>
     <div id="carouselExample" class="carousel slide">
       <div class="carousel-inner">
         <div class="carousel-item active">
-          <div class="d-block w-100"></div>
+          <div class="d-block w-100" style="background-color: gray; height: 600px;">gray</div>
         </div>
         <div class="carousel-item">
-          <div class="d-block w-100"></div>
+          <div class="d-block w-100" style="background-color: blue; height: 600px;">blue</div>
         </div>
         <div class="carousel-item">
-          <div class="d-block w-100"></div>
+          <div class="d-block w-100" style="background-color: red; height: 600px;">red</div>
         </div>
       </div>
       <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
---
+++ src/views/Home.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>{{ AA }}</div>
+        <div>{{ BB }}</div>
+        <div>{{ CC }}</div>
+        <div>{{ DD }}</div>
+        <div>{{ EE }}</div>
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator'
+
+@Component({
+    components: {}
+})
+export default class Home extends Vue {
+    private AA: string = 'aaaa';
+    private BB: string = 'bbbb'
+    private CC: string = 'cccc'
+    private DD: string = 'dddd'
+    private EE: string = 'eeee'
+}
+
+</script>
+
+<style lang="css" scoped>
+
+</style>
