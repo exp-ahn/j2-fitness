db673c1130a2cef4156c5c51c7a05404ccc6d6b3
exp-ahn <wltn24996@gmail.com>
2024-10-20 21:42:32 +0900

    axios 설치 및 Location 컴포넌트 내부 버튼 로고 수정

--- src/components/mainSection/Location.vue
+++ src/components/mainSection/Location.vue
@@ -12,8 +12,7 @@
                         class="map-button kakao-map-button"
                         @click="openNewTab('https://place.map.kakao.com/889644700')"
                     >
-                        <img src="/imgs/map-icons/kakao/kakaomap_basic.png" alt="카카오 맵"/>
-                        카카오 맵
+                        <img src="/imgs/map-icons/kakao/kakaomap_horizontal_ko.png" alt="카카오 맵"/>
                     </div>
                     <!-- 네이버 지도 링크가 폐업처리된 곳으로 나옴 이후 수정처리 필요함!! -->
                     <div
@@ -21,8 +20,7 @@
                         @click="openNewTab('https://map.naver.com/p/entry/place/1108438557?c=14.63,0,0,0,dh')"
                     >
                         <img src="/imgs/map-icons/naver/Map_Service_Icon.png" alt="네이버 지도" />
-                        <!-- <img src="./imgs/map-icons/naver/___Logotype________WH.png" alt="네이버 지도" /> -->
-                        네이버 지도
+                        <img src="/imgs/map-icons/naver/___Logotype________WH.png" alt="네이버 지도" />
                     </div>
                 </div>
                 <div class="detail-info">
