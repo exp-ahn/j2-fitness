426f8415091a39995818992c8b1d49058ca93a7d
exp-ahn <wltn24996@gmail.com>
2024-10-20 22:45:50 +0900

    footer css 1차 수정

--- src/components/HomeFooter.vue
+++ src/components/HomeFooter.vue
@@ -7,66 +7,68 @@
                         <img src="/imgs/j2-logos/j2_main_logo_1.png" alt="J2 FITNESS" />
                         <img src="/imgs/j2-logos/j2_main_logo_2.png" alt="J2 FITNESS" />
                     </div>
-                    <div class="contents-small-circles">
+                    <div class="contents-small-circles d-flex">
                         <div
-                            class="small-circle"
+                            class="small-circle d-flex justify-content-center"
                             @click="openNewTab('https://www.instagram.com/j2fitness_gaegeum/')"
                         >
                             <i class="fa-brands fa-instagram"></i>
                         </div>
-                        <div
+                        <!-- <div
                             class="small-circle"
                             @click="openNewTab('https://www.youtube.com/watch?v=iIrF75Kk6uQ')"
                         >
                             <i class="fa-brands fa-youtube"></i>
-                        </div>
+                        </div> -->
                         <div
-                            class="small-circle"
+                            class="small-circle d-flex justify-content-center"
                             @click="openNewTab('https://blog.naver.com/dmfmtm')"
                         >
-                            <!-- <img class="footer-naver-blog" src="./imgs/blog.png" /> -->
+                            <img class="footer-naver-blog" src="/imgs/contact/blog.png" />
                         </div>
                     </div>
                 </div>
                 <div class="footer-right">
-                    <div class="flex">
+                    <div class="d-flex mb-2">
                         <div class="footer-info-wrap">
-                            <h6 class="footer-info mb-2">예약 / 상담 / 문의</h6>
-                            <h5 class="footer-info mb-3">Tel. 051-891-8766</h5>
+                            <p class="text-light fw-bold fs-6 mb-2">예약 / 상담 / 문의</p>
+                            <p class="text-light fw-bold fs-5 mb-3">Tel. 051-891-8766</p>
                         </div>
-                        <div class="contact-small-circles">
-                            <div class="small-circle">
+                        <div class="contact-small-circles d-flex pt-3">
+                            <div class="small-circle d-flex justify-content-center">
                                 <i
                                     class="fa-solid fa-phone"
                                     ></i>
                                     <!-- onClick={() => handleCopyClipBoard('051-891-8766')} -->
                             </div>
-                            <div class="small-circle">
-                                <!-- <img
+                            <div class="small-circle d-flex justify-content-center">
+                                <img
                                     class="kakao-black"
-                                    src="./imgs/contact/kakao-black.png"
+                                    src="/imgs/contact/kakao-black.png"
                                     @click="openNewTab('https://open.kakao.com/o/s0OcyAhb')"
-                                /> -->
+                                />
                             </div>
-                            <div class="small-circle">
-                                <!-- <img
+                            <div class="small-circle  d-flex justify-content-center">
+                                <img
                                     class="naver-talk-black"
-                                    src="./imgs/contact/naver-talk-black.png"
+                                    src="/imgs/contact/naver-talk-black.png"
                                     @click="openNewTab('https://talk.naver.com/ct/wc2kmy?frm=mnmb&frm=nmb_detail')"
-                                /> -->
+                                />
                             </div>
                         </div>
                     </div>
-                    <div class="available-time">
-                        <p>상담 가능 시간</p>
-                        <p>평일 9:00 ~ 22:00</p>
+                    <div class="text-light">
+                        <p class="fs-6 mb-2">상담 가능 시간</p>
+                        <p class="fw-bold mb-3 fs-5">평일 9:00 ~ 22:00</p>
                     </div>
                 </div>
             </div>
-            <div class="footer-info">
+            <div class="text-light">
                 <p>대표 : 김성주</p>
                 <p>주소 : 부산 부산진구 가야대로 473 송원빌딩 B1F 제이투피트니스</p>
-                <p>Copyright© 2024 J2 FITNESS. All Rights Reserved.</p>
+                <p style="padding-bottom: 20px !important; margin-bottom: 0px !important;">
+                    Copyright© 2024 J2 FITNESS. All Rights Reserved.
+                </p>
             </div>
         </div>
     </footer>
@@ -84,6 +86,9 @@ export default class HomeFooter extends Vue {
 </script>
 
 <style lang="css" scoped>
+footer {
+    background-color: #000;
+}
 .small-circle {
     background-color: #fff;
     border-radius: 100%;
@@ -104,11 +109,6 @@ export default class HomeFooter extends Vue {
     object-fit: contain;
 }
 
-footer {
-    background-color: #000;
-    /* height: 290px; */
-}
-
 .footer-naver-blog {
     width: 30px;
     height: 30px;
@@ -126,25 +126,20 @@ footer {
     height: 33px;
     margin: 3.5px auto;
 }
-
 .fa-phone {
     font-size: 1.2em;
 }
-
 .fa-youtube {
     font-size: 1.5em;
 }
-
 .fa-instagram {
     font-size: 1.7em;
 }
-
 .footer-wrap {
     width: 1200px;
     margin: 0 auto;
     padding-top: 30px;
 }
-
 .footer-underline-box {
     display: flex;
     justify-content: space-between;
@@ -152,63 +147,31 @@ footer {
     padding-bottom: 20px;
     margin-bottom: 15px;
 }
-
 .footer-logo {
     display: flex;
     align-items: center;
     margin-bottom: 20px;
 }
-
 .footer-logo > img:first-child {
     width: 50px;
     object-fit: contain;
     margin-right: 10px;
 }
-
 .footer-logo > img:last-child {
     height: 50px;
     object-fit: contain;
 }
-
-.contents-small-circles,
-.contact-small-circles {
-    display: flex;
-}
-
-.contents-small-circles > div {
+.contents-small-circles > div,
+.contact-small-circles > div {
     margin-right: 20px;
 }
-
-.contact-small-circles > div {
-    margin-left: 20px;
-    margin-top: 20px;
-}
-
 .footer-right {
     width: 400px;
 }
-
-.footer-info {
-    text-align: left;
-    color: #fff;
-    margin-bottom: 1px;
-}
-
-h5.footer-info {
-    font-weight: bold;
-}
-
-.available-time {
-    text-align: left;
-    color: #fff;
-    margin-left: 6px;
-}
-
 .available-time > p:last-child {
     font-weight: bold;
     font-size: 1.1em;
 }
-
 .footer-info-wrap {
     padding-top: 5px;
     padding-right: 25px;
