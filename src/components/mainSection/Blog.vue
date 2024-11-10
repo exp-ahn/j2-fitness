c0738a7206a33915587a5cfd4305001ff1e57e9a
exp-ahn <wltn24996@gmail.com>
2024-11-10 22:03:19 +0900

    add Blog Component code for fixing build error

---
+++ src/components/mainSection/Blog.vue
@@ -0,0 +1,16 @@
+<template>
+    <section>
+
+    </section>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({
+    components: {}
+})
+export default class Blog extends Vue {
+
+}
+</script>
