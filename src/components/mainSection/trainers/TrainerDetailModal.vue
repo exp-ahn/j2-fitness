c9f6de6d6bca1e35a9e282bab5d60b53c99d7d31
exp_ahn <wltn24996@gmail.com>
2024-11-21 09:53:21 +0900

    Add Img Section in Trainer Modal Component

--- src/components/mainSection/trainers/TrainerDetailModal.vue
+++ src/components/mainSection/trainers/TrainerDetailModal.vue
@@ -1,13 +1,17 @@
 <template>
     <div v-if="isVisible" class="modal fade show" tabindex="-1" style="display: block;">
-        <div class="modal-dialog modal-dialog-centered" >
+        <div class="modal-dialog modal-lg modal-dialog-centered" >
             <div class="modal-content">
                 <div class="modal-header">
                     <h5 class="modal-title">{{ detailInfo.name }}</h5>
                     <button type="button" class="btn-close" @click="closeModal"></button>
                 </div>
-                <div class="modal-body detail-text text-start">
-                    {{ detailInfo.detail }}
+                <div class="modal-body detail-text text-start d-flex">
+                    <div>
+                        <img :src="trainerImg" alt="trainer_img" style="max-width: 400px; max-height: 550px">
+                    </div>
+                    <dir>{{ detailInfo.detail }}</dir>
+                    
                 </div>
             </div>
         </div>
@@ -33,6 +37,10 @@ export default class TrainerDetailModal extends Vue {
     @Prop({default: {}}) detailInfo!: TrainerInfo;
     @Prop({default: false}) isVisible!: boolean;
 
+    private get trainerImg() {
+        return this.detailInfo.imgAddress;
+    }
+
     private closeModal() {
         this.$emit('close');
     }
@@ -42,6 +50,5 @@ export default class TrainerDetailModal extends Vue {
 <style lang="css" scoped>
 .detail-text {
     white-space: pre-line;
-    
 }
 </style>
