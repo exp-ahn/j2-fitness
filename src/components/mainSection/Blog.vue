0b549d987e9246f0a941b2128f3d6b178da7d368
exp-ahn <wltn24996@gmail.com>
2024-11-24 00:02:29 +0900

    Axios get CORS error test code

--- src/components/mainSection/Blog.vue
+++ src/components/mainSection/Blog.vue
@@ -1,16 +1,20 @@
 <template>
-    <section>
+    <section class="text-center">
+        <h3>블로그</h3>
 
     </section>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
+import WebCrawler from '../common/crawling/Crawler';
 
 @Component({
     components: {}
 })
 export default class Blog extends Vue {
-
+    public mounted() {
+        WebCrawler.scrapeWithCheerio();
+    }
 }
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,15 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
-  filenameHashing: true
+  filenameHashing: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://m.blog.naver.com', // 프록시할 대상 URL
+        changeOrigin: true, // CORS 헤더를 설정
+        pathRewrite: { '^/api': '' }, // /api로 시작하는 경로를 제거
+        secure: false, // HTTPS 인증서를 무시
+      },
+    },
+  },
 })
